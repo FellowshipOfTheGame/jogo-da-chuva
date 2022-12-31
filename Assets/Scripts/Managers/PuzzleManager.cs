85235300612fd1b39b3d2c04cfe304af89563549
Tyago Teoi <tyago.teoi@usp.br>
2022-12-31 15:48:07 -0300

    Clean first play scene objts and create prefabs

--- Assets/Scripts/Managers/PuzzleManager.cs
+++ Assets/Scripts/Managers/PuzzleManager.cs
@@ -6,7 +6,7 @@ public class PuzzleManager : MonoBehaviour
     public static PuzzleManager Instance;
 
     [SerializeField] private GameObject _puzzle;
-    [SerializeField] private GameObject _scenario;
+    [SerializeField] private GameObject _scenario;  // Gameobject containing all scenario obj and its colliders
 
     void Awake()
     {
@@ -16,12 +16,14 @@ public class PuzzleManager : MonoBehaviour
             GameObject.Destroy(this);
     }
 
+    // Deactivate/activate all scenario colliders inside the '_scenario' object
     private void DeactivateAllDiscuptiveColliders(bool activation)
     {
         foreach (Collider2D col in _scenario.GetComponentsInChildren<Collider2D>())
             col.enabled = activation;
     }
 
+    // Pass to the next scene (called when a puzzle is solved)
     public void PuzzleSolved()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
